12bf8130e624ceb72dc007db0f8674d30bab343f
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-18 22:25:58 +0300

    Fix allocation bug in Encoder

--- lab1/src/main/java/Encoder.java
+++ lab1/src/main/java/Encoder.java
@@ -5,7 +5,7 @@ public class Encoder {
     public static byte[] encode(Package pg) {
         int wLen = pg.bMsg.message.getBytes().length + 8;
         byte bMagic = 0x13;
-        ByteBuffer bb = ByteBuffer.allocate(16 + wLen);
+        ByteBuffer bb = ByteBuffer.allocate(16 + wLen+2);
         bb.put(bMagic);
         bb.put(pg.bSrc);
         bb.putLong(pg.bPktId);
@@ -15,7 +15,7 @@ public class Encoder {
         bb.putInt(pg.bMsg.cType);
         bb.putInt(pg.bMsg.bUserId);
         bb.put(pg.bMsg.message.getBytes());
-        short wCrc162 = Crc16.calculateCrc(bb.array(), 16, 2+wLen);
+        short wCrc162 = Crc16.calculateCrc(bb.array(), 16, wLen);
         bb.putShort(wCrc162);
         return bb.array();
     }

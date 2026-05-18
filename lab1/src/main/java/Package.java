6545c44a797deb84bb49489bd8728f17af9f43e2
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-18 14:56:10 +0300

    Create Message as separate class

--- lab1/src/main/java/Package.java
+++ lab1/src/main/java/Package.java
@@ -3,11 +3,33 @@ public class Package {
     long bPktId;
     Message  bMsg;
 
+    public Package(byte bSrc, long bPktId, Message bMsg) {
+        this.bSrc = bSrc;
+        this.bPktId = bPktId;
+        this.bMsg = bMsg;
+    }
 
+    public byte getbSrc() {
+        return bSrc;
+    }
 
-    class Message{
-        int cType;
-        int bUserId;
-        String message;
+    public void setbSrc(byte bSrc) {
+        this.bSrc = bSrc;
+    }
+
+    public long getbPktId() {
+        return bPktId;
+    }
+
+    public void setbPktId(long bPktId) {
+        this.bPktId = bPktId;
+    }
+
+    public Message getbMsg() {
+        return bMsg;
+    }
+
+    public void setbMsg(Message bMsg) {
+        this.bMsg = bMsg;
     }
 }

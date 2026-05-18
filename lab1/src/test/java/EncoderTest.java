d5bef29680431aa8cde8574238cbf98cee810325
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-18 22:26:14 +0300

    Write tests for Encoder

--- lab1/src/test/java/EncoderTest.java
+++ lab1/src/test/java/EncoderTest.java
@@ -1,5 +1,70 @@
+import org.apache.commons.codec.binary.Hex;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class EncoderTest {
 
+    @Test
+    public void sameBehaviorOfEncoders() {
+Package pg = new Package((byte)0x33, 45, new Message(71, 51, "Secret")  );
+     byte[]encoded = Encoder.encode(pg);
+     assertEquals("1333000000000000002d0000000e227b00000047000000335365637265740000", Hex.encodeHexString(encoded));
+    }
+
+    @Test
+    public void samePackagesAreEncodedTheSame() {
+        Package pg1 = new Package((byte)0x33, 45, new Message(100, 51, "Secret")  );
+        Package pg2 = new Package((byte)0x33, 45, new Message(100, 51, "Secret")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertArrayEquals(encoded1, encoded2);
+    }
+
+    @Test
+    public void differentCTypeResultsInDifferentPackageEncoding() {
+        Package pg1 = new Package((byte)0x33, 45, new Message(50, 51, "Secret")  );
+        Package pg2 = new Package((byte)0x33, 45, new Message(100, 51, "Secret")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertFalse(Arrays.equals(encoded1, encoded2));
+    }
+
+    @Test
+    public void differentUserIDResultsInDifferentPackageEncoding() {
+        Package pg1 = new Package((byte)0x33, 45, new Message(50, 0, "Secret")  );
+        Package pg2 = new Package((byte)0x33, 45, new Message(50, 100, "Secret")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertFalse(Arrays.equals(encoded1, encoded2));
+    }
+
+    @Test
+    public void differentMessageResultsInDifferentPackageEncoding() {
+        Package pg1 = new Package((byte)0x33, 45, new Message(50, 100, "Secret1")  );
+        Package pg2 = new Package((byte)0x33, 45, new Message(50, 100, "Secret2")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertFalse(Arrays.equals(encoded1, encoded2));
+    }
+
+    @Test
+    public void differentBPktIdResultsInDifferentPackageEncoding() {
+        Package pg1 = new Package((byte)0x33, 50, new Message(50, 100, "Secret")  );
+        Package pg2 = new Package((byte)0x33, 10, new Message(50, 100, "Secret")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertFalse(Arrays.equals(encoded1, encoded2));
+    }
+
+    @Test
+    public void differentBSrcResultsInDifferentPackageEncoding() {
+        Package pg1 = new Package((byte)0x10, 10, new Message(50, 100, "Secret")  );
+        Package pg2 = new Package((byte)0x33, 10, new Message(50, 100, "Secret")  );
+        byte[]encoded1 = Encoder.encode(pg1);
+        byte[]encoded2 = Encoder.encode(pg2);
+        assertFalse(Arrays.equals(encoded1, encoded2));
+    }
 }

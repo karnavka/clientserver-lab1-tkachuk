716ecbbb3383bcab7c678b84dd02df984fe0e445
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-26 21:52:38 +0300

    Remake DataBase and commands to only have products in groups

--- hw2/src/main/java/enums/Commands.java
+++ hw2/src/main/java/enums/Commands.java
@@ -2,11 +2,12 @@ package enums;
 
 public enum Commands {
     GET_PRODUCT_QUANTITY(0),
-    DEL_PRODUCT(1),
-    ADD_PRODUCT(2),
+    DEL_PRODUCT_AMOUNT(1),
+    ADD_PRODUCT_AMOUNT(2),
     ADD_GROUP(3),
     ADD_PRODUCT_TO_GROUP(4),
-    SET_PRICE(5);
+    SET_PRICE(5),
+    CALLBACK(6);
 
     public final int cType;
 

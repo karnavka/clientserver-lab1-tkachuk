864c9ebfc329d5dc1cabce3dc812a1eea7fbc296
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-26 21:50:03 +0300

    Remake DataBase and commands to only have products in groups

--- hw2/src/main/java/utils/DataBase.java
+++ hw2/src/main/java/utils/DataBase.java
@@ -1,15 +1,19 @@
 package utils;
 
 import enteties.Group;
+import enteties.Product;
 import enums.Groups;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DataBase {
     public static List<Group> products = new ArrayList<>();
 
     public DataBase() {
-        products.add(new Group(Groups.NO_GROUP.name));
+        groups.put(Groups.NO_GROUP.name, new ConcurrentHashMap<>());
     }
 }

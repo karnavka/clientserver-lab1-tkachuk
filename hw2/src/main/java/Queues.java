4f7a176f27d7fcc675fb1c18fa05f7a8a5423e84
olyaaaa <olechka.tkachukk@gmail.com>
2026-05-26 20:17:26 +0300

    Fill queues class

--- hw2/src/main/java/Queues.java
+++ hw2/src/main/java/Queues.java
@@ -1,2 +1,13 @@
+import packet.Package;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Queues {
+    public static final BlockingQueue<byte[]> queueOfRawInfo = new LinkedBlockingQueue<>();
+    public static final BlockingQueue<Package> queueOfPackages = new LinkedBlockingQueue<>();
+    public static final BlockingQueue<Package> queueOfAnswers = new LinkedBlockingQueue<>();
+    public static final BlockingQueue<Package> queueOfEncryptedAnswers = new LinkedBlockingQueue<>();
+
+
 }

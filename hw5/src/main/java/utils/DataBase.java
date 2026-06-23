c3a664ffe144141bd5aae294ff1f426452225326
olyaaaa <olechka.tkachukk@gmail.com>
2026-06-24 00:54:46 +0300

    Add http

--- hw5/src/main/java/utils/DataBase.java
+++ hw5/src/main/java/utils/DataBase.java
@@ -2,6 +2,7 @@ package utils;
 
 import enteties.Product;
 import service.ProductService;
+import service.UserService;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,8 +14,20 @@ public class DataBase {
     private static final String DEFAULT_DB_URL = "jdbc:sqlite:store.db";
     private static final ProductService PRODUCT_SERVICE =
             new ProductService(DEFAULT_DB_URL);
+    private static final UserService USER_SERVICE =
+            new UserService(DEFAULT_DB_URL);
+
+    static {
+        // seed default users if they don't exist yet
+        USER_SERVICE.createUser("admin", "admin123");
+        USER_SERVICE.createUser("user", "password");
+    }
 
     public static ProductService getProductService() {
         return PRODUCT_SERVICE;
+    }
+
+    public static UserService getUserService() {
+        return USER_SERVICE;
     }
 }
